1e27bad2f25a375efb4606baa0f48fae94ec65e8
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 11:05:59 +0800

    [B2] ADD BRANCH2

---
+++ src/main/CalculatorB2.java
@@ -0,0 +1,29 @@
+package main;
+
+public class CalculatorB2 {
+
+	public static int add(int a, int b) {
+		return a + b + b + b;
+	}
+
+	public static int add2(int a, int b) {
+		return a + b;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b * b;
+	}
+
+	public static int multiply2(int a, int b) {
+		return a * b * b;
+	}
+
+	public static int multiply4(int a, int b) {
+		return a * b * b;
+	}
+
+	public static void div() {
+	}
+
+
+}
