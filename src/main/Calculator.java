ec4072e7b88e2cb6e201aae133cc708d0e2b2855
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 10:39:32 +0800

    [Master] add add2

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
 		return a + b + b + b;
 	}
 
+	public static int add2(int a, int b) {
+		return a + b;
+	}
+
 	public static int multiply(int a, int b) {
 		return a * b * b;
 	}
