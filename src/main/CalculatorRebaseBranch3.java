1a19cadb67f7f243f3f19a09b3727c9b2fb0bf4f
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 11:12:39 +0800

    [B3] ADD ADD4

--- src/main/CalculatorRebaseBranch3.java
+++ src/main/CalculatorRebaseBranch3.java
@@ -10,6 +10,9 @@ public class CalculatorRebaseBranch3 {
 		return a + b;
 	}
 
+	public static int add4(int a, int b) {
+		return a + b;
+	}
 	public static int multiply(int a, int b) {
 		return a * b * b;
 	}
