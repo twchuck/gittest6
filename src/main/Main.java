99aa6f13034b42a9b32044094f171f235ea6ab3f
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 11:10:19 +0800

    [Master] add master

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(Calculator.add(1, 2));
 		System.out.println(Calculator.multiply(1, 2));
+		System.out.println(Calculator.add(1, 2));
+
 	}
 
 }
