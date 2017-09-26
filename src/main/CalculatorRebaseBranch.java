5b2195d5f365c22d6eb3ad45b73abf2ea91bafee
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 10:42:39 +0800

    [Rebase] add rebase branch file

---
+++ src/main/CalculatorRebaseBranch.java
@@ -0,0 +1,17 @@
+package main;
+
+public class CalculatorRebaseBranch {
+
+	public static int add(int a, int b) {
+		return a + b + b + b;
+	}
+
+	public static int add2(int a, int b) {
+		return a + b;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b * b;
+	}
+
+}
