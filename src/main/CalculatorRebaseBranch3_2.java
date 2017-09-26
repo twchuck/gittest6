b007e284ec93214f96cfd7c5f6177d3eeffa13eb
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 11:14:23 +0800

    [B3-2] ADD BRANCH 3_2

---
+++ src/main/CalculatorRebaseBranch3_2.java
@@ -0,0 +1,23 @@
+package main;
+
+public class CalculatorRebaseBranch3_2 {
+
+	public static int add2(int a, int b) {
+		return a + b;
+	}
+
+	public static int add3(int a, int b) {
+		return a + b;
+	}
+
+	public static int add4(int a, int b) {
+		return a + b;
+	}
+	public static int multiply(int a, int b) {
+		return a * b * b;
+	}
+
+	public static int multiply2(int a, int b) {
+		return a * b * b;
+	}
+}
