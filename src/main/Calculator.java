a8f68459c434a1dbd44912e83281c6ef8aeb2f33
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 11:13:15 +0800

    [Master] ADD ADD4

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -9,9 +9,15 @@ public class Calculator {
 	public static int add2(int a, int b) {
 		return a + b;
 	}
+
 	public static int add3(int a, int b) {
 		return a + b;
 	}
+
+	public static int add4(int a, int b) {
+		return a + b;
+	}
+
 	public static int multiply(int a, int b) {
 		return a * b * b;
 	}
@@ -26,6 +32,5 @@ public class Calculator {
 
 	public static void div() {
 	}
-
 
 }
