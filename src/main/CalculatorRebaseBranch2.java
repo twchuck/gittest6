b181047395a29a4ef22817cdb439b11ba3148278
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 10:51:52 +0800

    [Rebase2] add rebase branch2

---
+++ src/main/CalculatorRebaseBranch2.java
@@ -0,0 +1,13 @@
+package main;
+
+public class CalculatorRebaseBranch2 {
+
+	public static int add2(int a, int b) {
+		return a + b;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b * b;
+	}
+
+}
