d9b32fd7eea7608aabfeef97aa7f313ec4f65073
chucklee <chuck@liminghandeMacBook-Pro.local>
2017-09-26 10:53:28 +0800

    [Master] add div2

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -13,13 +13,18 @@ public class Calculator {
 	public static int multiply(int a, int b) {
 		return a * b * b;
 	}
+
 	public static int multiply2(int a, int b) {
 		return a * b * b;
 	}
+
 	public static int multiply4(int a, int b) {
 		return a * b * b;
 	}
-	
+
 	public static void div() {
+	}
+
+	public static void div2() {
 	}
 }
